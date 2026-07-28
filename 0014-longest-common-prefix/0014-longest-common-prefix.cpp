1825b18435172023669535cbb483aa1a1ed002ed
Vaishnavi Gupta <vaishnavigu2005@gmail.com>
2026-07-28 17:35:59 +0530

    Time: 0 ms (100%), Space: 11.7 MB (79.4%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        // if array is empty i.e no prefix
-        if(strs.empty()) {
+        
+        if(strs.empty()){
             return "";
         }
-        // strating with whole strs[0] as prefix
-        string prefix= strs[0];
 
-        for(int i=1; i<strs.size(); i++){
-            // if strs [i] doesnt have prefix then will reduse prefix by removing last character
-            while(strs[i].find(prefix)!=0){
-                prefix= prefix.substr(0, prefix.size()-1);
-                // if prefix is empty i.e no prefix
-                if(prefix.empty()){return "";}
+        for(int i=0; i<strs[0].size(); i++){
+            char c= strs[0][i];
+
+            for(int j=1; j< strs.size(); j++){
+
+                if(i==strs[j].size()|| strs[j][i]!=c){
+                    return strs[0].substr(0,i);
+                }
 
             }
-
         }
-        
-        return prefix;
+        return strs[0];
                
     }
 };
