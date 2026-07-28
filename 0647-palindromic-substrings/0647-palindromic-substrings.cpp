fb42510a1fd4828ba09d00b404d126786e0ad30f
Vaishnavi Gupta <vaishnavigu2005@gmail.com>
2026-07-28 16:20:58 +0530

    Time: 3 ms (85.66%), Space: 8.4 MB (99.36%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n= s.size();
+        int count=0;
+        for(int center=0; center< 2*n-1; center++){
+            int left= center/2;
+            int right= left+ (center%2);
+
+            while(left>=0 && right<n && s[left]==s[right]){
+                count++;
+                left--;
+                right++;
+            }
+        }
+        return count;
+        
+    }
+};
